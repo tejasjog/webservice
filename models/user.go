785201c4e3c0b4ea58eeda7682a37629abd30015
Tejas Jog <tejas.jog@gmail.com>
2021-07-21 18:39:06 +0530

    functions 6

--- models/user.go
+++ models/user.go
@@ -10,3 +10,14 @@ var (
 	users  []*User
 	nextID = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(user User) (User, error) {
+	user.ID = nextID
+	nextID++
+	users = append(users, &user)
+	return user, nil
+}
