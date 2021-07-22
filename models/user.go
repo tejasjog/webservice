3152af09d740ea786608dc780507beead77bd2d9
Tejas Jog <tejas.jog@gmail.com>
2021-07-22 12:26:18 +0530

    webservice 7

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,8 +21,40 @@ func GetUsers() []*User {
 }
 
 func AddUser(user User) (User, error) {
+	if user.ID != 0 {
+		return User{}, errors.New("ID for new user must be 0")
+	}
 	user.ID = nextID
 	nextID++
 	users = append(users, &user)
 	return user, nil
+}
+
+func GetUserByID(ID int) (User, error) {
+	for _, u := range users {
+		if u.ID == ID {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("no user found with ID %v", ID)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("no user found with ID %v", u.ID)
+}
+
+func RemoveUserByID(ID int) error {
+	for i, u := range users {
+		if u.ID == ID {
+			users = append(users[:i], users[1+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("no user found with ID %v", ID)
 }
